975f2a7a463ec9aaf5391de63e4d55d180b37aee
xyesoskh <azamatdul112233@gmail.com>
2025-05-16 05:15:03 +0300

    Update oz.py

--- oz.py
+++ oz.py
@@ -9,7 +9,7 @@ import asyncio
 # Настройки для Telethon
 api_id = 29459757  # Твой API ID
 api_hash = '7cc969764c4de8a52169570ac20000a8'  # Твой API Hash
-session_string = "1ApWapzMBu4Sbmc7c5s44pLQ22UEse-Uyc0U0xWkxcOshY..."  # 🔁 ВСТАВЬ СЮДА ПОЛНЫЙ session_string
+session_string = "1ApWapzMBu4Sbmc7c5s44pLQ22UEse-Uyc0U0xWkxcOshYoED_Fb71Sq54idI6hqSNWQVG_gCDQhnUQVAFd_fQMcbRbNWvmoqDM4uS02q-RTcvwQT3mDOGcabfPwYaPV8oXtHfNTOHHY8vukH6NP7gSUBA4itvhpGn74nC1SfngevCA_LfGpeoOtN_jZDMG_zlWtlpAHxlJl6w5zS7qIR6kwSvD-HfKBCKlHOAdgMndoFEda47mrj35Glz1v7OVgFcv2RhxKPWCOkcynMIwLDpDyCjj1k_1zr_LtAuaDgrLKJVz0h5Khj7122_7b0H2kypMDzGOp5fqATdKt5KeS3PzkawZENeH0="  # 🔁 ВСТАВЬ СЮДА ПОЛНЫЙ session_string
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
